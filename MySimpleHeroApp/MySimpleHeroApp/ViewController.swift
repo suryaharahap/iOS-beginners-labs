bad85bdb3a79f19c3c33a86777ffd6444442240b
suryaharahap <suryaharahap18@gmail.com>
2021-08-16 15:21:11 +0700

    Static table view

---
+++ MySimpleHeroApp/MySimpleHeroApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySimpleHeroApp
+//
+//  Created by Surya on 16/08/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
